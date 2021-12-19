f72792776fa7c7921e34627694fb4b0ab66f9232
mmjck <msantosjck@gmail.com>
2021-12-18 22:10:18 -0400

    Initial Commit

---
+++ iChat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iChat
+//
+//  Created by Jackson Matheus dos Santos on 18/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Jackson Matheus dos Santos on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
